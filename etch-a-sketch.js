05a5184d8590402ef9f7290b377e6c059e43b0a6
John <141792618+michaellucas2008@users.noreply.github.com>
2024-07-24 13:32:12 -0400

    add a logic that darkens the background color of an element whenever the mouse hover over it

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -13,6 +13,9 @@ for(let i=0; i<16;i++){
             if(backColor==='rgba(0, 0, 0, 0)'){
                 gridItem.style.backgroundColor=getRandomColor();
             }
+            else{
+                gridItem.style.backgroundColor=getDarkerColor(gridItem);
+            }
             
         });
         gridArray[i].appendChild(gridItem);
@@ -23,7 +26,28 @@ function getRandomColor(){
     const red = Math.floor(Math.random()*256);
     const green= Math.floor(Math.random()*256);
     const blue = Math.floor(Math.random()*256);
-    const alpha = Math.floor(Math.random()*256);
-    return `rgb(${red}, ${green}, ${blue}, ${alpha})`;
+    return `rgba(${red}, ${green}, ${blue}, 1)`;
 }
+function getDarkerColor(gridItem){
+    const rgbValues=window.getComputedStyle(gridItem).backgroundColor.match(/\d+/g);
+    const backColor=String(window.getComputedStyle(gridItem).backgroundColor);
+    const rgbArray=new Array(3);
+    let result='';
+    if(backColor==='rgb(0, 0, 0)'){
+        result = 'rgba(0, 0, 0, 1)'
+    }
+    else{
+        console.log("this is from the else statement");
+        for(let i=0;i<rgbValues.length;i++){
+            rgbArray[i]=rgbValues[i]-14;// reduce color by 10%
+            if(rgbArray[i]<0){ 
+                rgbArray[i]=0;//if the rgb values of the element is negative, its set to 0.
+            }
+        }
+        result = `rgba(${rgbArray[0]}, ${rgbArray[1]}, ${rgbArray[2]}, 1)`;
+    }
+    
+    return result;
+}
+
 
