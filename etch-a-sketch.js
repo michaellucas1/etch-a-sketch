9c33b014fc4678edb2f94a03fc8c9fdcdd9e1075
John <141792618+michaellucas2008@users.noreply.github.com>
2024-07-23 23:41:08 -0400

    add event listener to each grid element and set a random color when the mouse hover to it

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,9 +6,20 @@ for(let i=0; i<16;i++){
     for(let j=0;j<16;j++){
         const gridItem = document.createElement("div");
         gridItem.classList.add("grid-item");
-        gridItem.textContent=`${j+1}`;
-        gridArray[i].appendChild(gridItem);   
+        gridArray[i].appendChild(gridItem); 
+        gridItem.addEventListener("mouseover",(event)=>{
+            event.stopImmediatePropagation();
+            gridItem.style.backgroundColor=getRandomColor();
+        });  
+        gridArray[i].appendChild(gridItem);
     }
     gridContainer.appendChild(gridArray[i]);
 }
-    
+
+function getRandomColor(){
+    const red = Math.floor(Math.random()*256);
+    const green= Math.floor(Math.random()*256);
+    const blue = Math.floor(Math.random()*256);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
