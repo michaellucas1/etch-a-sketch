b8224e5900cc8a2db159c689b83ec26a4110e853
John <141792618+michaellucas2008@users.noreply.github.com>
2024-07-23 21:45:27 -0400

    refactor the logic to add 16 more rows of div elements to the grid container

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,14 @@
 const gridContainer = document.querySelector(".grid-container");
-for(let i=1;i<=16;i++){
-    const gridItem = document.createElement("div");
-    gridItem.classList.add("grid-item");
-    gridItem.textContent=`${i}`;
-    gridContainer.append(gridItem);
+for(let i=0; i<16;i++){
+    const gridArray=new Array(16);
+    gridArray[i]= document.createElement("div");
+    gridArray[i].classList.add("grid-row");
+    for(let j=0;j<16;j++){
+        const gridItem = document.createElement("div");
+        gridItem.classList.add("grid-item");
+        gridItem.textContent=`${j+1}`;
+        gridArray[i].appendChild(gridItem);   
+    }
+    gridContainer.appendChild(gridArray[i]);
 }
+    
